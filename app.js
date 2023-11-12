23fc143a45d138b42bbe166a540397de06d7df74
Darien McIntosh <darienmcintosh14@gmail.com>
2023-11-11 22:40:44 -0500

    Added updated files

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("searchButton").addEventListener("click", function () {
-       var xhr = new XMLHttpRequest();
-       xhr.onreadystatechange = function () {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
                     alert("List of Superheroes:\n" + xhr.responseText);
                 } else {
+                    console.error("Error fetching data. Status: " + xhr.status);
                     alert("Error fetching data");
                 }
             }
         };
-        xhr.open("GET", "superheroes.php", true);
+        xhr.open("GET", "http://localhost/info2180-lab4/superheroes.php", true);
         xhr.send();
     });
 });
